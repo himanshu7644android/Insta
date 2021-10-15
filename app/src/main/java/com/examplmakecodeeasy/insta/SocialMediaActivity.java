3508698d4da31d742c7727864c0a0c9fef4adea4
himanshu <codewithhk607@gmail.com>
2021-10-15 10:09:45 +0530

    SocialMediaActivity and three Fragent Added

--- app/src/main/java/com/examplmakecodeeasy/insta/SocialMediaActivity.java
+++ app/src/main/java/com/examplmakecodeeasy/insta/SocialMediaActivity.java
@@ -1,17 +1,25 @@
 package com.examplmakecodeeasy.insta;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.widget.TableLayout;
+import android.widget.Toolbar;
 
 public class SocialMediaActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private ViewPager viewPager;
+    private TableLayout tabLayout;
+    private TabAdapter tabAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_social_media);
 
-        
+
     }
 
 
