7286b979e013ef419976ee8dd5db682003117e3a
himanshu <codewithhk607@gmail.com>
2021-10-17 13:26:53 +0530

    Tab Layout SetUp

--- app/src/main/java/com/examplmakecodeeasy/insta/SocialMediaActivity.java
+++ app/src/main/java/com/examplmakecodeeasy/insta/SocialMediaActivity.java
@@ -3,21 +3,45 @@ package com.examplmakecodeeasy.insta;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+
 import android.os.Bundle;
 import android.widget.TableLayout;
-import android.widget.Toolbar;
+
+import com.google.android.material.tabs.TabLayout;
+
 
 public class SocialMediaActivity extends AppCompatActivity {
 
-    private Toolbar toolbar;
+   // private Toolbar toolbar;
     private ViewPager viewPager;
-    private TableLayout tabLayout;
     private TabAdapter tabAdapter;
+    private androidx.appcompat.widget.Toolbar toolbar;
+   // private  TableLayout tabLayout;
+   private TabLayout tabLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_social_media);
+
+        setTitle("Social Media App!!");
+
+        toolbar = findViewById(R.id.mytoolbar);
+        setSupportActionBar(toolbar);
+
+        viewPager = findViewById(R.id.viewPager);
+        tabAdapter = new TabAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(tabAdapter);
+
+        tabLayout = findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager,false);
+
+
+
+
+
+
 
 
     }
